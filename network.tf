9336b3273b64fa74c9bccc97561b64529e83d781
Mosh <mosh_shood@hotmail.com>
2025-08-24 10:43:40 +0100

    Refactor the network to use dynamic rules

--- network.tf
+++ network.tf
@@ -1,182 +1,126 @@
-resource "random_pet" "lb_hostname" {
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
+# VNET + Subnet
 resource "azurerm_virtual_network" "vnet" {
-  name                = "terraformvnet"
+  name                = "${local.prefix}-vnet"
   address_space       = [element(var.network_config, 0)]
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = local.tags
 }
-
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "subnet"
+  name                 = "${local.prefix}-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["${element(var.network_config, 1)}/${element(var.network_config, 2)}"]
+  address_prefixes     = [local.subnet_cidr]
 }
 
-resource "azurerm_network_security_group" "myNSG" {
-  name                = "myNSG"
+# NSG with dynamic rules
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.nsg_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  tags                = local.tags
 
-
-  # Allow inbound from Azure Load Balancer (health probes + LB flows)
-  security_rule {
-    name                       = "allow-loadbalancer-inbound"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "AzureLoadBalancer"
-    destination_address_prefix = "*"
+  dynamic "security_rule" {
+    for_each = var.nsg_rules
+    content {
+      name                       = security_rule.value.name
+      priority                   = security_rule.value.priority
+      direction                  = security_rule.value.direction
+      access                     = security_rule.value.access
+      protocol                   = security_rule.value.protocol
+      source_port_range          = security_rule.value.source_port_range
+      destination_port_range     = security_rule.value.destination_port_range
+      source_address_prefix      = security_rule.value.source_address_prefix
+      destination_address_prefix = security_rule.value.destination_address_prefix
+    }
   }
-
-  # Allow SSH via Load Balancer NAT (LB frontend IP -> backend port 22)
-  security_rule {
-    name                       = "allow-ssh-via-lb-nat"
-    priority                   = 150
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = azurerm_public_ip.public_ip.ip_address
-    destination_address_prefix = "*"
-  }
-
-  # Deny everything else inbound
-  security_rule {
-    name                       = "deny-all-inbound"
-    priority                   = 200
-    direction                  = "Inbound"
-    access                     = "Deny"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  # Allow all outbound (typical for backend VMs)
-  security_rule {
-    name                       = "allow-all-outbound"
-    priority                   = 100
-    direction                  = "Outbound"
-    access                     = "Allow"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-
 }
 
-
-
-resource "azurerm_subnet_network_security_group_association" "myNSG" {
+resource "azurerm_subnet_network_security_group_association" "assoc" {
   subnet_id                 = azurerm_subnet.subnet.id
-  network_security_group_id = azurerm_network_security_group.myNSG.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
-#A public Ip address for the load balancer
-resource "azurerm_public_ip" "public_ip" {
-  name                = "lb-publicIP"
+# Public IP + Load Balancer
+resource "random_pet" "lb_hostname" {}
+
+resource "azurerm_public_ip" "lb" {
+  name                = "${local.prefix}-lb-pip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
   sku                 = "Standard"
-  zones               = null
-  domain_name_label   = substr(lower(replace("${azurerm_resource_group.rg.name}-${random_pet.lb_hostname.id}", "_", "-")), 0, 63)
+  domain_name_label   = substr(lower(replace("${local.prefix}-${random_pet.lb_hostname.id}", "_", "-")), 0, 63)
+  tags                = local.tags
 }
 
-# A load balancer with a frontend Ip configuration and a backend pool 
-resource "azurerm_lb" "mylb" {
-  name                = "myLB"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_lb" "lb" {
+  name                = var.lb_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
   sku                 = "Standard"
+  tags                = local.tags
 
   frontend_ip_configuration {
-    name                 = "myPublicIP"
-    public_ip_address_id = azurerm_public_ip.public_ip.id
+    name                 = var.lb_frontend_ip_name
+    public_ip_address_id = azurerm_public_ip.lb.id
   }
 }
 
-resource "azurerm_lb_backend_address_pool" "backend_pool_address" {
-  name            = "myBackendAddressPool"
-  loadbalancer_id = azurerm_lb.mylb.id
+resource "azurerm_lb_backend_address_pool" "backend" {
+  name            = var.lb_backend_pool_name
+  loadbalancer_id = azurerm_lb.lb.id
 }
 
-# set up load balacer rule from azurerm_lb.mylb front end ip to azurerm_lb_backend_address_pool.backendpooladdress backend ip port 80 to port 80
-resource "azurerm_lb_rule" "lb_rule" {
-  name                           = "http"
-  loadbalancer_id                = azurerm_lb.mylb.id
-  protocol                       = "Tcp"
-  frontend_port                  = 80
-  backend_port                   = 80
-  frontend_ip_configuration_name = "myPublicIP"
-  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.backend_pool_address.id]
-  probe_id                       = azurerm_lb_probe.lb_probe.id
-}
-
-#set up load balance probe to check if the backend is up
-resource "azurerm_lb_probe" "lb_probe" {
+resource "azurerm_lb_probe" "http" {
   name            = "http-probe"
-  loadbalancer_id = azurerm_lb.mylb.id
+  loadbalancer_id = azurerm_lb.lb.id
   protocol        = "Http"
   port            = 80
   request_path    = "/"
 }
 
-resource "azurerm_lb_nat_rule" "ssh" {
-  name                           = "ssh"
-  resource_group_name            = azurerm_resource_group.rg.name
-  loadbalancer_id                = azurerm_lb.mylb.id
+resource "azurerm_lb_rule" "http" {
+  name                           = "http-rule"
+  loadbalancer_id                = azurerm_lb.lb.id
   protocol                       = "Tcp"
-  frontend_port_start            = 50000
-  frontend_port_end              = 50119
-  backend_port                   = 22
-  frontend_ip_configuration_name = "myPublicIP"
-  backend_address_pool_id        = azurerm_lb_backend_address_pool.backend_pool_address.id
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = var.lb_frontend_ip_name
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.backend.id]
+  probe_id                       = azurerm_lb_probe.http.id
 }
 
-resource "azurerm_public_ip" "natgwpip" {
-  name                = "natgw-publicIP"
+# NAT Gateway
+resource "azurerm_public_ip" "nat" {
+  name                = "${local.prefix}-nat-pip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
   sku                 = "Standard"
-  zones               = null
+  tags                = local.tags
 }
 
-# add nat gateway to enable outbound traffic from the backend instances
-resource "azurerm_nat_gateway" "nat_gateway" {
-  name                    = "nat-Gateway"
-  location                = azurerm_resource_group.rg.location
-  resource_group_name     = azurerm_resource_group.rg.name
-  sku_name                = "Standard"
-  idle_timeout_in_minutes = 10
-  zones                   = null
+resource "azurerm_nat_gateway" "nat" {
+  name                = var.nat_gateway_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name            = "Standard"
+  tags                = local.tags
 }
 
-resource "azurerm_subnet_nat_gateway_association" "nat_gateway_association" {
+resource "azurerm_nat_gateway_public_ip_association" "nat_assoc" {
+  public_ip_address_id = azurerm_public_ip.nat.id
+  nat_gateway_id       = azurerm_nat_gateway.nat.id
+}
+
+resource "azurerm_subnet_nat_gateway_association" "nat_subnet_assoc" {
   subnet_id      = azurerm_subnet.subnet.id
-  nat_gateway_id = azurerm_nat_gateway.nat_gateway.id
-}
-
-# add nat gateway public ip association
-resource "azurerm_nat_gateway_public_ip_association" "nat_gateway_public_ip_association" {
-  public_ip_address_id = azurerm_public_ip.natgwpip.id
-  nat_gateway_id       = azurerm_nat_gateway.nat_gateway.id
+  nat_gateway_id = azurerm_nat_gateway.nat.id
 }
