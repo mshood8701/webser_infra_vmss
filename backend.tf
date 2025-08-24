01a93f9f148b7d367eacd5aef60610f6ff7893c7
Mosh <mosh_shood@hotmail.com>
2025-08-24 10:40:15 +0100

    refactor the backend file

--- backend.tf
+++ backend.tf
@@ -1,10 +1,7 @@
 terraform {
   backend "azurerm" {
     use_azuread_auth     = true
-    tenant_id            = ""                      # Set via ARM_TENANT_ID environment variable
-    client_id            = ""                      # Set via ARM_CLIENT_ID environment variable
-    client_secret        = ""                      # Set via ARM_CLIENT_SECRET environment variable
-    storage_account_name = "train11733"            # Pass via -backend-config or env var
+    storage_account_name = "train11567"            # Pass via -backend-config
     container_name       = "tfstate"               # Pass via -backend-config
     key                  = "dev.terraform.tfstate" # Pass via -backend-config
   }
