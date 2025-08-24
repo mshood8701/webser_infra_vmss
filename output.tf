1d381e1b1e7487c2d39933bcea0cc8e8e5dae13b
Mosh <mosh_shood@hotmail.com>
2025-08-24 10:45:27 +0100

    Tp prevent repeated values and tags

--- output.tf
+++ output.tf
@@ -1,6 +1,19 @@
-output "application_endpoint" {
+/*output "application_endpoint" {
   value = "http://${azurerm_public_ip.public_ip.fqdn}/index.php"
 }
 
 
 /* value = "http://${azurerm_public_ip.na.fqdn}/index.php */
+
+
+output "lb_public_ip" {
+  value = azurerm_public_ip.lb.ip_address
+}
+
+output "lb_dns" {
+  value = azurerm_public_ip.lb.fqdn
+}
+
+output "vmss_name" {
+  value = azurerm_orchestrated_virtual_machine_scale_set.web_server.name
+}
