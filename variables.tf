b1ef1036714ac5d3ce36dca6222c84f6a6291e65
Mosh <mosh_shood@hotmail.com>
2025-08-24 10:46:45 +0100

    Refactor and configure all resources

--- variables.tf
+++ variables.tf
@@ -1,54 +1,59 @@
 variable "env" {
   description = "Deployment environment (dev, staging, prod)"
   type        = string
+  default     = "dev"
 }
 
 variable "resource_group_name" {
+  description = "Resource Group name"
   type        = string
-  description = "The cointainer to hold all resource"
-  default     = "kml_rg_main-1a21c94512594f06"
+  default     = "kml_rg_main-7ef002048c3c41fc"
 }
 
 variable "location" {
+  description = "Azure region"
   type        = string
-  description = "The location where resource are provision"
   default     = "westus"
 }
 
 variable "network_config" {
+  description = "Network Configurations [vnet_cidr, subnet_prefix, subnet_mask]"
   type        = tuple([string, string, number])
-  description = "Network Configurations"
   default     = ["192.168.0.0/16", "192.168.2.0", 24]
 }
-
 
 variable "lb_name" {
   description = "Name of the Load Balancer"
   type        = string
+  default     = "loadbalancer"
 }
 
 variable "lb_backend_pool_name" {
   description = "Name of the backend pool for Load Balancer"
   type        = string
+  default     = "myBackendAddresspool"
 }
 
 variable "lb_frontend_ip_name" {
   description = "Name of the frontend IP configuration"
   type        = string
+  default     = "lb_frontend_ip"
 }
 
 variable "nat_gateway_name" {
-  description = "Name of the NAT Gateway for outbound connectivity"
+  description = "Name of the NAT Gateway"
   type        = string
+  default     = "nat_gateway"
 }
 
 variable "nsg_name" {
   description = "Name of the Network Security Group"
   type        = string
+  default     = "nsg"
 }
 
 variable "nsg_rules" {
-  description = "List of NSG rules"
+  description = "NSG rules list"
   type = list(object({
     name                       = string
     priority                   = number
@@ -60,9 +65,11 @@ variable "nsg_rules" {
     source_address_prefix      = string
     destination_address_prefix = string
   }))
+
+  # Default: allow LB, allow SSH, deny-all inbound, allow-all outbound
   default = [
     {
-      name                       = "Allow-LB"
+      name                       = "allow-lb"
       priority                   = 100
       direction                  = "Inbound"
       access                     = "Allow"
@@ -71,30 +78,61 @@ variable "nsg_rules" {
       destination_port_range     = "*"
       source_address_prefix      = "AzureLoadBalancer"
       destination_address_prefix = "*"
+    },
+    {
+      name                       = "allow-ssh-via-lb-nat"
+      priority                   = 150
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "22"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "deny-all-inbound"
+      priority                   = 200
+      direction                  = "Inbound"
+      access                     = "Deny"
+      protocol                   = "*"
+      source_port_range          = "*"
+      destination_port_range     = "*"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "allow-all-outbound"
+      priority                   = 100
+      direction                  = "Outbound"
+      access                     = "Allow"
+      protocol                   = "*"
+      source_port_range          = "*"
+      destination_port_range     = "*"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
     }
   ]
 }
 
-variable "state_rg" {
-  description = "Resource Group for storing Terraform backend state"
-  type        = string
-}
-
-variable "state_storage_account" {
-  description = "Storage Account for Terraform backend state"
-  type        = string
-}
-
-variable "state_container" {
-  description = "Blob Container for Terraform backend state"
-  type        = string
-}
-
-
 variable "tags" {
   description = "Common tags applied to all resources"
   type        = map(string)
-  default = {
-    modified_on = formatdate("YYYY_MM_DD", timestamp())
-  }
+  default     = {}
+
+}
+
+
+
+
+variable "storage_account_name" {
+  type        = string
+  description = "The Storage account for the blob"
+  default     = "train11567"
+}
+
+variable "container_name" {
+  type        = string
+  description = "The container for the tfstate file"
+  default     = "tfstate"
 }
