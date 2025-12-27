f9f764368d942a2016a6ccf7f7ccd1d2da6ff2d6
M <mosh_shood@hotmail.com>
2025-12-27 14:16:36 +0000

    Add HTTP allow rule to security group

--- variables.tf
+++ variables.tf
@@ -69,6 +69,17 @@ variable "nsg_rules" {
   # Default: allow LB, allow SSH, deny-all inbound, allow-all outbound
   default = [
     {
+  name                       = "allow-http"
+  priority                   = 110
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "80"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+},
+    {
       name                       = "allow-lb"
       priority                   = 100
       direction                  = "Inbound"
